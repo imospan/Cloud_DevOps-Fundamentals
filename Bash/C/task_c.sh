fec3d5be18a1c6e1f68baa66e28fd4791a50b581
Ivan Mospan <106439773+imospan@users.noreply.github.com>
2023-01-29 16:17:51 +0200

    Update task_c.sh

--- Bash/C/task_c.sh
+++ Bash/C/task_c.sh
@@ -3,13 +3,13 @@
 #Checking for correct arguments
 if [ ! -d "$1" ] || [ ! -d "$2" ]; then
 	echo "You need to pass two parameters"
-  echo "1. The path to the directory where the copies of the files will be stored."
-  echo "2. Path to the syncing  directory."
+	echo "1. Path to the syncing  directory."
+	echo "2. The path to the directory where the copies of the files will be stored."
 	exit
 fi
 
-DESTINATION=$1
-SOURCE=$2
+SOURCE=$1
+DESTINATION=$2
 LOG=$DESTINATION/file.log
 
 #Creating backup directory and adding timestapm to log
@@ -17,9 +17,8 @@ if ! (mkdir $DESTINATION 2> /dev/null)
 then
 	echo "Directory already exists" >> $LOG
 else
-  echo "Creating directory" >> $LOG
+	echo "Creating directory" >> $LOG
 fi
-echo "Timestamp is done $(date +"%D %T")" >> $LOG
 
 #Copying files and adding results to the log
 echo "Copying Files" >> $LOG
